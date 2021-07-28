f4ef15d7b3735dca9ca05299a1db5e3fb8d3513b
Oyedee <oyempemia@gmail.com>
2021-07-28 14:06:37 +0100

    Resume to the SharedPreferenceThree Activity after the user inputs their details

--- app/src/main/java/com/example/sharedpreferencesapp/SharedPreferenceTwo.java
+++ app/src/main/java/com/example/sharedpreferencesapp/SharedPreferenceTwo.java
@@ -19,6 +19,13 @@ public class SharedPreferenceTwo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shared_preference_two);
 
+        SharedPreferences sharedPreferences = getSharedPreferences("DisplayValues", MODE_PRIVATE);
+
+        if (sharedPreferences.contains("VALID")) {
+            startActivity( new Intent(SharedPreferenceTwo.this, SharedPreferenceThree.class));
+        }
+
+
         EditText name = findViewById(R.id.edit_text);
         EditText email = findViewById(R.id.edit_text2);
         EditText address = findViewById(R.id.edit_text3);
@@ -32,6 +39,7 @@ public class SharedPreferenceTwo extends AppCompatActivity {
                 editor.putString("NAME_VALUE", name.getText().toString());
                 editor.putString("EMAIL_VALUE", email.getText().toString());
                 editor.putString("ADDRESS_VALUE", address.getText().toString());
+                editor.putBoolean("VALID", true);
                 editor.apply();
 
                 Intent intent = new Intent(SharedPreferenceTwo.this, SharedPreferenceThree.class);
