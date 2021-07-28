f62746d486bf5123e11601e456f7c6c4e2c585a4
Oyedee <oyempemia@gmail.com>
2021-07-28 15:32:47 +0100

    Final implementation for SharedPreference - count the number of times the user opens you app

---
+++ app/src/main/java/com/example/sharedpreferencesapp/SharedPreferenceFour.java
@@ -0,0 +1,20 @@
+package com.example.sharedpreferencesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SharedPreferenceFour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared_preference_four);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+    }
+}
